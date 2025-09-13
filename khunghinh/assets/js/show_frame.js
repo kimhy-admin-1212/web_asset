6a6f3b5bdfb42f90da81936f89cfc1ea9e7a304d
ntphat19pm <ntphat.19pm@gmail.com>
2025-09-13 14:31:17 +0700

    khunghinh

--- khunghinh/assets/js/show_frame.js
+++ khunghinh/assets/js/show_frame.js
@@ -149,8 +149,8 @@ function saveAsImage() {
   html2canvas(transparentDiv.get(0), {
     scrollX: 500,
     scale: scaleResponsive,
-    width: 336.6,
-    height: 336.6,
+    width: 450.5,
+    height: 450.5,
   }).then(function (canvas) {
     canvas.toBlob(function (blob) {
       var url = URL.createObjectURL(blob);
